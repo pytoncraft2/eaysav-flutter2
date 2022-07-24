4217767cd74b318bcc25160f7b0decc8b4daf333
pytoncraft2 <pythoncraft2@gmail.com>
2022-07-24 20:20:09 +0200

    [~] début page equipement avec liste

--- lib/acceuil_page.dart
+++ lib/acceuil_page.dart
@@ -193,24 +193,3 @@ class PhotosList extends StatelessWidget {
     );
   }
 }
-
-class SecondRoute extends StatelessWidget {
-  const SecondRoute({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Second Route'),
-      ),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            // Navigate back to first route when tapped.
-          },
-          child: const Text('Go back!'),
-        ),
-      ),
-    );
-  }
-}
--- lib/equipements_page.dart
+++ lib/equipements_page.dart
@@ -1,10 +1,58 @@
+// import 'package:flutter/material.dart';
+
+// class Equipements extends StatelessWidget {
+//   const Equipements({Key? key}) : super(key: key);
+
+//   @override
+//   Widget build(BuildContext context) {
+//     Scaffold(
+//       appBar: AppBar(
+//         title: const Text('Second Route'),
+//       ),
+//       body: Center(
+//           child: ListView(
+//         children: const <Widget>[
+//           ListTile(
+//             leading: Icon(Icons.map),
+//             title: Text('Map'),
+//           ),
+//           ListTile(
+//             leading: Icon(Icons.photo_album),
+//             title: Text('Album'),
+//           ),
+//           ListTile(
+//             leading: Icon(Icons.phone),
+//             title: Text('Phone'),
+//           ),
+//         ],
+//       )),
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
+
+const int itemCount = 10;
 
 class Equipements extends StatelessWidget {
   const Equipements({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Text("Equipements");
+    return Scaffold(
+      body: ListView.builder(
+        itemCount: itemCount,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+            title: Text('Item ${(index + 1)}'),
+            leading: const Icon(Icons.access_alarm),
+            trailing: const Icon(Icons.account_circle),
+            onTap: () {
+              debugPrint('Tapage sur index numero ${(index)}');
+            },
+          );
+        },
+      ),
+    );
   }
 }
