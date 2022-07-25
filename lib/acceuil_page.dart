d3e6cbe0a9d0e48a726680e4f476cbcdcd5bdbbb
pytoncraft2 <pythoncraft2@gmail.com>
2022-07-25 23:55:04 +0200

    [~]

--- lib/acceuil_page.dart
+++ lib/acceuil_page.dart
@@ -139,18 +139,12 @@ List<Photo> parsePhotos(String responseBody) {
 class Photo {
   final String libelle;
   final String maSlug;
-  Property property;
 
-  const Photo(
-      {required this.libelle,
-      required this.maSlug,
-      required Property property});
+  const Photo({required this.libelle, required this.maSlug});
 
   factory Photo.fromJson(Map<String, dynamic> json) {
     return Photo(
-        libelle: json['libelle'] as String,
-        maSlug: json['maSlug'] as String,
-        property: Property.fromJson(json['property']));
+        libelle: json['libelle'] as String, maSlug: json['maSlug'] as String);
   }
 }
 
@@ -196,29 +190,5 @@ class PhotosList extends StatelessWidget {
         );
       },
     );
-  }
-}
-
-class Shape {
-  String shapeName;
-  Property property;
-
-  Shape({required this.shapeName, required this.property});
-
-  factory Shape.fromJson(Map<String, dynamic> parsedJson) {
-    return Shape(
-        shapeName: parsedJson['shape_name'],
-        property: Property.fromJson(parsedJson['property']));
-  }
-}
-
-class Property {
-  double width;
-  double breadth;
-
-  Property({required this.width, required this.breadth});
-
-  factory Property.fromJson(Map<String, dynamic> json) {
-    return Property(width: json['width'], breadth: json['breadth']);
   }
 }
