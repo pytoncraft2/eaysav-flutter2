b432b0a1f3d310df2d7ed55c5b31a53034b92c62
pytoncraft2 <pythoncraft2@gmail.com>
2022-07-26 22:08:18 +0200

    [~] drawer de base le retour

--- lib/main.dart
+++ lib/main.dart
@@ -53,16 +53,46 @@ class _RootPageState extends State<RootPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-          title: const Text("EasySAV 🔥"),
+      appBar: AppBar(
+        title: const Text("EasySAV 🔥"),
+      ),
+      body: pages[pageCourante],
+      drawer: Drawer(
+        // Add a ListView to the drawer. This ensures the user can scroll
+        // through the options in the drawer if there isn't enough vertical
+        // space to fit everything.
+        child: ListView(
+          // Important: Remove any padding from the ListView.
+          padding: EdgeInsets.zero,
+          children: [
+            const DrawerHeader(
+              decoration: BoxDecoration(
+                color: Colors.blue,
+              ),
+              child: Text('Drawer Header'),
+            ),
+            ListTile(
+              title: const Text('Item 1'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+            ListTile(
+              title: const Text('Item 2'),
+              onTap: () {
+                // Update the state of the app
+                // ...
+                // Then close the drawer
+                Navigator.pop(context);
+              },
+            ),
+          ],
         ),
-        body: pages[pageCourante],
-        drawer: const Drawer(
-          child: SingleChildScrollView(
-            padding: EdgeInsets.all(16.0),
-            child: Text("OKAY"),
-          ),
-        ));
+      ),
+    );
   }
   // return Scaffold(
   //   appBar: AppBar(
